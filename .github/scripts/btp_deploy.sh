5c66ccfdd92498b3e06bc425eec0436c3d73f937
fahrialmd <a.fahrialhamdi@gmail.com>
2025-11-11 11:09:18 +0700

    restore application.yaml

---
+++ .github/scripts/btp_deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo "1. Install CF"
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
+echo "deb https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+sudo apt-get update
+sudo apt-get install cf8-cli
+
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo "2. Check CF Installation"
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+cf -v
+
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo "3. Install CF Plugins"
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+cf add-plugin-repo CF-Community https://plugins.cloudfoundry.org
+cf install-plugin multiapps -f
+cf install-plugin html5-plugin -f
+
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo "4. Build fiori apps"
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+npx mbt build --mtar app.mtar
+
+echo '############## Authorizations ##############'
+cf api $cf_api_url
+cf auth $cf_user "$cf_password"
+
+echo '############## Deploy ##############'
+cf target -o $cf_org -s $cf_space
+cf deploy mta_archives/app.mtar -f
